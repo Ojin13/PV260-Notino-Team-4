0647b47fa3b98b3800d05ec771f8b462059eaf7d
Adam Haluska <adam.haluska3@gmail.com>
2026-04-23 22:31:46 +0200

    Add CreateDiffCommand

---
+++ Popocatepetl.Application/Commands/CreateReportsDiffCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Popocatepetl.Application.Commands;
+
+public record CreateReportsDiffCommand(
+    Guid baselineReportId,
+    Guid currentReportId
+) : IRequest<Unit>;
---
+++ Popocatepetl.Application/Handlers/CreateReportsDiffCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Popocatepetl.Application.Commands;
+using Popocatepetl.Domain.Interfaces;
+
+namespace Popocatepetl.Application.Handlers;
+
+public sealed class CreateReportsDiffCommandHandler(
+    IDiffResultRepository diffResultRepository,
+    Guid baselineReportId,
+    Guid currentReportId
+) : IRequestHandler<CreateReportsDiffCommand, Unit>
+{
+    public Task<Unit> Handle(CreateReportsDiffCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
