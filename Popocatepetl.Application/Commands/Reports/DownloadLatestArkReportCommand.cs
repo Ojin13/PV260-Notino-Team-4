477b69565c545870ab690ac63906617a6dedbe7e
Adam Haluska <adam.haluska3@gmail.com>
2026-04-24 14:00:07 +0200

    Add download event logging

--- Popocatepetl.Application/Commands/Reports/DownloadLatestArkReportCommand.cs
+++ Popocatepetl.Application/Commands/Reports/DownloadLatestArkReportCommand.cs
@@ -5,4 +5,9 @@ using Popocatepetl.Application.Dtos;
 namespace Popocatepetl.Application.Commands.Reports;
 
 /// <summary>Downloads and stores the latest ARK report.</summary>
-public sealed record DownloadLatestArkReportCommand : IRequest<Result<DownloadLatestArkReportResponse>>;
+public sealed record DownloadLatestArkReportCommand : IAuditableRequest,
+    IRequest<Result<DownloadLatestArkReportResponse>>
+{
+    public string ActionName => "DownloadLatestReport";
+    public string Detail => "";
+}
