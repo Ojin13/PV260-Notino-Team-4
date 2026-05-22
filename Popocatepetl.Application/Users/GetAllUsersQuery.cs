c022ff4a6ef2c8baa2d65968f3165f2477613e89
Adam Haluska <adam.haluska3@gmail.com>
2026-05-22 19:58:18 +0200

    Remove user related commands and tests

--- Popocatepetl.Application/Users/GetAllUsersQuery.cs
+++
@@ -1,7 +0,0 @@
-using MediatR;
-using Popocatepetl.Domain.Entities;
-
-namespace Popocatepetl.Application.Users;
-
-/// <summary>Returns all registered users.</summary>
-public record GetAllUsersQuery : IRequest<IReadOnlyList<AppUser>>;
