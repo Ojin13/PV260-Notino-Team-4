a87569a18a388af776914759982540e28b353f43
Adam Haluska <adam.haluska3@gmail.com>
2026-05-22 20:36:15 +0200

    Add 'remove user' migration

---
+++ Popocatepetl.Infrastructure/Data/Migrations/20260522183235_RemoveAppUser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Popocatepetl.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveAppUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+    }
+}
